947e1acb44900e446e98b3185463ad2be78f4a4b
Minh Le <minh.le@mgisolutions.com>
2023-08-28 13:11:50 +0700

    sidebar

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let path = '';
+  export let tooltip = '';
+</script>
+
+<div class="btn transition">
+  <svg aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d={path}></path></svg>
+  <p class="tooltip">{tooltip}</p>
+</div>
+<style scoped>
+  .btn {
+    @apply relative flex items-center justify-center bg-slate-400 w-16 h-16 mt-4 rounded-[128px];
+  }
+  .btn:hover {
+    @apply text-white rounded-2xl bg-green-500;
+  }
+  .tooltip {
+    @apply invisible absolute text-white left-24 min-w-[150px] py-1.5 px-2.5 rounded-xl bg-slate-600 opacity-0;
+    -webkit-transition: all 1s ease-in-out;
+    transition: all 1s ease-in-out;
+  }
+  .btn:hover .tooltip {
+    @apply visible opacity-100;
+    -webkit-transition: all 0.75s ease-in-out;
+    transition: all 0.75s ease-in-out;
+  }
+  .transition {
+      /* -webkit-transition: background-color 0.75s linear, border-radius 0.75s linear;
+      transition: background-color 0.75s linear, border-radius 0.75s linear; */
+      -webkit-transition: all 0.5s ease-in-out;
+      transition: all 0.5s ease-in-out;
+  }
+</style>
---
+++ src/lib/components/SideBar.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Button from "./Button.svelte";
+  import { icCompass, icDownload, icPlus, icClock } from "$lib/Icons/icons";
+
+  const listMenus = [
+    {
+      icon: icCompass,
+      tooltip: 'Icon Compass'
+    },
+    {
+      icon: icDownload,
+      tooltip: 'Icon Download'
+    },
+    {
+      icon: icPlus,
+      tooltip: 'Icon Plus'
+    },
+    {
+      icon: icClock,
+      tooltip: 'Icon Clock'
+    }
+  ]
+</script>
+
+<div class="fixed top-0 bottom-0 left-0 w-28 bg-slate-600 flex flex-col items-center justify-start">
+  <div class="mt-10">
+    {#each listMenus as menu}
+      <Button tooltip={menu.tooltip} path={menu.icon} />
+    {/each}
+  </div>
+</div>
